62c9d122c6b5887354ae83b830884f3ed4a50113
Franz Pletz <fpletz@fnordicwalking.de>
2024-02-29 23:54:37 +0100

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/068d4db604958d05d0b46c47f79b507d84dbc069' (2024-02-29)
      → 'github:NixOS/nixpkgs/9099616b93301d5cf84274b184a3a5ec69e94e08' (2024-02-28)
    • Updated input 'poetry2nix':
        'github:fpletz/poetry2nix/8d55f0ad2e70121e554d23f54481e1a77c4887f1' (2024-02-05)
      → 'github:nix-community/poetry2nix/3c92540611f42d3fb2d0d084a6c694cd6544b609' (2024-02-22)
    • Updated input 'poetry2nix/flake-utils':
        'github:numtide/flake-utils/ff7b65b44d01cf9ba6a71320833626af21126384' (2023-09-12)
      → 'github:numtide/flake-utils/1ef2e671c3b0c19053962c07dbda38332dcebf26' (2024-01-15)
    • Updated input 'poetry2nix/nix-github-actions':
        'github:nix-community/nix-github-actions/4bb5e752616262457bc7ca5882192a564c0472d2' (2023-11-03)
      → 'github:nix-community/nix-github-actions/5163432afc817cf8bd1f031418d1869e4c9d5547' (2023-12-29)
    • Updated input 'poetry2nix/treefmt-nix':
        'github:numtide/treefmt-nix/e82f32aa7f06bbbd56d7b12186d555223dc399d1' (2023-11-12)
      → 'github:numtide/treefmt-nix/e504621290a1fd896631ddbc5e9c16f4366c9f65' (2024-02-19)

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "VATSIM python module";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     poetry2nix = {
-      url = "github:fpletz/poetry2nix/ruff-watchfiles-prevent-ifd";
+      url = "github:nix-community/poetry2nix";
       inputs = {
         nixpkgs.follows = "nixpkgs";
       };
@@ -13,46 +13,48 @@
   };
 
   outputs = inputs:
-  inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-    systems = [
-      "x86_64-linux"
-      "aarch64-linux"
-    ];
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
 
-    perSystem = { self, pkgs, system, config, lib, ... }: let
-      pkgs' = pkgs.extend(inputs.poetry2nix.overlays.default);
-      overrides = pkgs'.poetry2nix.overrides.withDefaults(final: prev: {});
-      python-vatsim = pkgs'.poetry2nix.mkPoetryApplication {
-        inherit overrides;
-        python = pkgs'.python311;
-        projectDir = pkgs'.poetry2nix.cleanPythonSources { src = ./.; };
-      };
-      python-vatsim-dev = pkgs'.poetry2nix.mkPoetryEnv {
-        inherit overrides;
-        python = pkgs'.python311;
-        pyproject = ./pyproject.toml;
-        poetrylock = ./poetry.lock;
-        extraPackages = ps: [ ps.ipython ];
-      };
-    in {
-      packages = {
-        default = python-vatsim;
-        inherit python-vatsim;
-      };
+      perSystem = { self, pkgs, system, config, lib, ... }:
+        let
+          pkgs' = pkgs.extend (inputs.poetry2nix.overlays.default);
+          overrides = pkgs'.poetry2nix.overrides.withDefaults (final: prev: { });
+          python-vatsim = pkgs'.poetry2nix.mkPoetryApplication {
+            inherit overrides;
+            python = pkgs'.python311;
+            projectDir = pkgs'.poetry2nix.cleanPythonSources { src = ./.; };
+          };
+          python-vatsim-dev = pkgs'.poetry2nix.mkPoetryEnv {
+            inherit overrides;
+            python = pkgs'.python311;
+            pyproject = ./pyproject.toml;
+            poetrylock = ./poetry.lock;
+            extraPackages = ps: [ ps.ipython ];
+          };
+        in
+        {
+          packages = {
+            default = python-vatsim;
+            inherit python-vatsim;
+          };
 
-      formatter = pkgs.nixpkgs-fmt;
+          formatter = pkgs.nixpkgs-fmt;
 
-      devShells.default = python-vatsim-dev.env.overrideAttrs (oldAttrs: {
-        nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [
-          pkgs'.nil
-          pkgs'.poetry
-          pkgs'.nodejs  # for pyright
-        ];
-        shellHook = ''
-          export POETRY_VIRTUALENVS_IN_PROJECT=true
-          export PYTHONPATH=${python-vatsim-dev}/${python-vatsim-dev.sitePackages}
-        '';
-      });
+          devShells.default = python-vatsim-dev.env.overrideAttrs (oldAttrs: {
+            nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [
+              pkgs'.nil
+              pkgs'.poetry
+              pkgs'.nodejs # for pyright
+            ];
+            shellHook = ''
+              export POETRY_VIRTUALENVS_IN_PROJECT=true
+              export PYTHONPATH=${python-vatsim-dev}/${python-vatsim-dev.sitePackages}
+            '';
+          });
+        };
     };
-  };
 }
